d0c5234b0ad4247baedf1bd111997a5f086d97cf
Andrew Davisson <adavisson@Andrews-MacBook-Air.local>
2019-10-02 20:27:52 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+function submitData(username, userEmail){
+  const userData = {
+    name: username,
+    email: userEmail
+  };
+
+  const configObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": 'applicaton/json',
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(submitData)
+  };
+
+  return fetch("http://localhost:3000/users")
+    .then(resp => resp.json())
+    .then(json => console.log(json));
+}
