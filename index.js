4befa503364a3e1a1a235fcba5d7743b6649d898
tcrurup <tcrurup@gmail.com>
2019-10-03 20:18:32 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+import { create } from "domain";
+
 // Add your code here
+
+
+function submitData(userName, userEmail){
+    
+    let formData = {
+        name: userName,
+        email: userEmail
+    }
+    
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch('http://localhost:3000/users', configObj)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(object){
+        createElementAndAppendToBody(object.id);
+    })
+    .catch(function(error){
+        createElementAndAppendToBody(error.message);
+    })
+}
+
+function createElementAndAppendToBody(innerHTML){
+    let newNode = document.createElement('p');
+    newNode.innerHTML = innerHTML;
+    document.querySelector("body").appendChild(newNode);
+}
