d27caff752bc98931f847cf7045053e235cd7c22
Rachel <williamsrachelada@gmail.com>
2019-10-04 17:37:38 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    let p = document.createElement('p')
+    p.innerHTML = object.id
+    document.body.appendChild(p)
+  })
+  .catch(function(error) {
+    let p = document.createElement('p');
+    p.innerHTML = error.message;
+    document.body.appendChild(p);
+  });
+}
