c3cede76c3d4e75fc17aa5d3cacb41e1d3a02cb2
Tsunamins <reillyamr@gmail.com>
2019-10-16 14:11:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,97 @@
 // Add your code here
+// configurationObject = {
+//     method: "POST", 
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//       }, 
+//       body: JSON.stringify({
+//         dogName: "Byron",
+//         dogBreed: "Poodle"
+//       })
+//   };
+
+//   fetch(destinationURL, configurationObject);
+
+//what the above is, or turns out to be, approximately:
+// fetch("http://localhost:3000/dogs", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify({
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   })
+// });
+
+
+//but also written as, approimately:
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   };
+   
+//   let configObj = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+//   };
+   
+//   fetch("http://localhost:3000/dogs", configObj);
+
+  //but then changed to, approximately:
+//   let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   };
+   
+  // method: "POST" is missing from the object below
+//   let configObj = {
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+//   };
+   
+//   fetch("http://localhost:3000/dogs", configObj)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(object) {
+//       console.log(object);
+//     })
+//     .catch(function(error) {
+//       alert("Bad things! Ragnarők!");
+//       console.log(error.message);
+//     });
+
+
+//however needs to be more like this:
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
