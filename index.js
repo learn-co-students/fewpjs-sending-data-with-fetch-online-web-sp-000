7b5022ebac74984feef752d541ea1f2d43a945e4
donna <donnacamos88@gmail.com>
2019-10-18 11:33:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+let submitData = {
+    name: "Donna",
+    email: "donna@donna.com"
+  };
+   
+  // method: "POST" is missing from the object below
+  let configObj = {
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(submitData) 
+  };
+   
+  fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log(object);
+    })
+    .catch(function(error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);
+    }); 
