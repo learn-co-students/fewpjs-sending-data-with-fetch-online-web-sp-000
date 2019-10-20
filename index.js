f3d9ebe5498dc6379722995364fba50092d2df14
davisjustinw <davisjustinw@gmail.com>
2019-10-20 15:24:55 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+const submitData = ( name, email ) => {
+  let formData = {
+    name: name,
+    email: email
+  }
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify( formData )
+  }
+  return fetch ( 'http://localhost:3000/users', configObj )
+    .then( response => response.json() )
+    .then( obj => {
+      let element = document.createElement( 'p' )
+      element.textContent = obj.id
+      document.querySelector('body').appendChild( element )
+    })
+    .catch( error => {
+      let element = document.createElement( 'p' )
+      element.textContent = error.message
+      document.querySelector('body').appendChild( element )
+    })
+}
