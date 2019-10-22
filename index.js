d72ca7fc0fdcfa14aef2d552325fca83d6cb207a
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-10-22 11:36:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your code here
+function submitData(name, email) {
+  const formData = {
+    name: name,
+    email: email
+  };
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then((response) => { return response.json(); })
+    .then((json) => { document.querySelector('body').innerHTML += `<h1>User ID: ${json.id}</h1>` })
+    .catch((error) => { document.querySelector('body').innerHTML += `<h1>Error: ${error.message}</h1>` });
+}
