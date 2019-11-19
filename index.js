d03245899147e5474136cd3ecc0c09efc61fb65c
Adil Naimi <adilnm23@hotmail.com>
2019-11-18 23:43:00 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(name,email) {
+  let formData = {
+  name: name,
+  email: email
+  };
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+  body: JSON.stringify(formData)
+}
+
+return fetch("http://localhost:3000/users", configObj)
+.then(function(response) {
+    return response.json()
+  })
+  .then(function(object) {
+    let div=document.createElement('div')
+    document.body.innerHTML=object.id
+  })
+.catch(function(error) {
+  document.body.innerHTML=error.message;
+})
+}
+
+
+submitData("Adil","adilnm23@gmail.com")
