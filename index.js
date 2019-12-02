f4091a3ebc098e3c67921d194b1e5cf5f6cf8194
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-12-01 17:26:31 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+const body = document.querySelector("body")
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`
+        })
+    }).then(function(resp) {
+        return resp.json();
+    }).then(function(object) {
+        console.log(object),
+        body.innerHTML = object.id;
+    }).catch(function(error) {
+        console.log(error.message);
+        body.innerHTML = error.message;
+    });
+};
