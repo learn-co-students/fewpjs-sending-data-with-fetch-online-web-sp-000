367d5858fab3ff2b18bf74463867346628116a38
treuther <tracyreuther@gmail.com>
2019-12-19 18:59:42 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email) {
+    let formData = {
+        name,
+        email
+    };
+
+    let configobj = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch('http://localhost:3000/users', configobj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            document.body.innerHTML = data.id;
+        })
+
+    .catch(function(error) {
+        document.body.innerHTML = error.message;
+    });
+}
