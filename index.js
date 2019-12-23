375d5e2840f380057a4dbd12b611d3d177f4a03e
chanwkkk <chanwk18@hotmail.com>
2019-12-23 14:16:32 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name,email){
+  let configurationObject = {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email:email
+    })
+  }
+  fetch('http://localhost:3000/users',configurationObject)
+    .then(resp=>resp.json())
+    .then(function(json){
+      console.log(json.id)
+      document.body.innerHTML = json.id
+    })
+}
+
+function addJsonToDom(json){
+   console.log(json.id)
+  
+}
