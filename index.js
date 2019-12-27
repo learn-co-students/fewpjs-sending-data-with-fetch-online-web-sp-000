e16c3d3ac84e8390ae040afe2fc0ea30e72656d6
boom100100 <bsd231@nyu.edu>
2019-12-27 12:29:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Add your code here
+function submitData(name, email){
+  /*let formData = {
+    name: username,
+    email: email
+  };*/
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify( {
+        name,
+        email
+      } )
+  };
+
+  return fetch('http://localhost:3000/users', configObj)
+  .then(function(response) {
+    //appendIt(response.json().id);
+    return response.json();
+  })
+  .then(function(object) {
+    //appendIt(object.id);
+    //return object["id"];
+    //id = object["id"];
+    appendIt(object.id);
+  })
+  .catch(function(error) {
+    appendIt(error.message);
+  });
+}
+
+function appendIt(it){
+  document.body.innerHTML = it;/*
+  const divElement = document.createElement('div');
+  divElement.innerHTML = `${it}`;
+  document.body.append(divElement);*/
+  //document.querySelector('body').innerHTML = `${it}`;
+}
+
+//submitData();
