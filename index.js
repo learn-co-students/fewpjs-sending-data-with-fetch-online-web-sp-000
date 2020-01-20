3ae7db0277382310664ed111a456b8ea545048d5
wyao905 <warrenyca905@gmail.com>
2020-01-19 21:23:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+let configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+};
+
+function submitData(userName, userEmail) {
+    configurationObject.body = JSON.stringify({name: userName, email: userEmail});
+    return fetch("http://localhost:3000/users", configurationObject)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(object) {
+                let userId = document.createElement("p");
+                let body = document.querySelector("body");
+                userId.innerHTML = object.id;
+                body.appendChild(userId);
+            })
+            .catch(function(error) {
+                let msg = document.createElement("p");
+                let body = document.querySelector("body");
+                msg.innerHTML = error.message;
+                body.appendChild(msg);
+            });
+}
