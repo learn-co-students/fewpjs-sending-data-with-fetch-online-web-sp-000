72b3caba7b45eec261c072b4b2b7c272d2738e1d
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-01-22 06:39:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    let p = document.createElement("p");
+    p.innerText = object.id;
+  })
+  .catch(function(error) {
+    console.log(error.message);
+  })
+}
