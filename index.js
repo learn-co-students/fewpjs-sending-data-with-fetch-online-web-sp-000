2ba0b79aefb22cde47b757d3b64660be5b258364
Paul Rosero <paul_rosero@yahoo.com>
2020-02-12 21:17:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 function submitData(name, email) {
     let users = {name: "Steve", email: "steve@steve.com"}
-    let obj = {method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify(users)}
+    let obj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(users)
+    }
 
     return fetch("http://localhost:3000/users", obj) 
-        .then(function(response) {
-            return response.json();
-          })
-          .then(function(object) {
-            document.body.innerHTML = object.id
-          })
-          .catch(function(error) {
-        
-            document.body.innerHTML = error.message;
-          })    
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        document.body.innerHTML = object.id
+    })
+    .catch(function(error) {    
+        document.body.innerHTML = error.message;
+    })    
 }
