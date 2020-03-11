61df0a73255ce33450a9b5f63e1944df35f29c33
Eric Clark <eclark0013@gmail.com>
2020-03-11 15:28:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 // Add your code here
 function submitData(name, email){
     let configObj = {
+        method: "POST",
         headers: {
             "Content-Type": "application/json",
             "Accept": "application/json"
-        }
+        },
         body: JSON.stringify({
             "name": name,
             "email": email
         })
     }
-    return fetch(http://localhost:3000/users, configObj)
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            document.querySelector("body").append(object["id"])
+        })
+        .catch(function(error) {
+            alert("Bad things! Ragnarők!");
+            document.querySelector("body").append(error.message);
+        })
+        ;
 }
+
+
