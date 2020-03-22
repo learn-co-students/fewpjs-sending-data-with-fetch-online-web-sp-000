87c235d85984df15d2932eb9b53d5bf1e864df90
Lauren McCoy <lm_lutec@outlook.com>
2020-03-22 09:56:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Add your code here
+
+function submitData(name, email){
+
+    let formData = {
+        name: name,
+        email: email
+    };
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    fetch("http://localhost:3000/users", configObj)
+        .then(function(response){
+        return response.json();
+        })
+        .then(function(json){
+            
+        //    let box = document.createElement("UL")
+        //    let item = document.createElement("LI")
+        let newEle = document.createElement("p")
+            newEle.innerText = json.id
+            document.body.appendChild(newEle)
+            
+
+            //box.appendChild(item)
+        })
+
+        .catch(function(error) {
+            document.body.appendChild(error.message);
+          });
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    submitData(name, email);
+});
+
