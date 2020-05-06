f9466c119ead457b76400d591aec1ccede5d21e1
azbury <acasbury25@aol.com>
2020-05-05 21:47:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData (name, email) {
+    let formData = {
+        name: name,
+        email: email
+      };
+
+    let configObj = {
+        method: "Post",
+        headers: {
+          "content-type": "application/json",
+          "accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+
+    return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      let element = document.createElement('h1')
+      element.innerHTML = object["id"]
+      document.querySelector('body').appendChild(element)
+    })
+    .catch(function(error) {
+      document.body.innerHTML = error.message
+    });
+}
