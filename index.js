dc4776ed9ecf981d73a6176437ed6d31d06c62be
Andrew-J-Williams <1williamsaj@gmail.com>
2020-06-17 19:58:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+    
+    let configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email 
+        })
+    };
+
+   return fetch("http://localhost:3000/users", configObject) // Remember to include return!
+        .then(function(response) {
+            return response.json(); // Return our response
+        })
+        .then(function(object) {
+            document.body.innerHTML = object["id"]
+        })
+        .catch(function(error) {
+            alert("Unauthorized Access");
+            document.body.innerHTML = error.message
+      });
+}
