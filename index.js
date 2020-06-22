98a2053709ac5c0fa9583fdd91fecf1c875c1d7d
AzrafCh <daragan1991@live.com>
2020-06-21 22:09:40 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+let submitData = {
+    name = "Byron",
+    email = "Poodle"
+};
+
+let configObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(submitData)
+};
+
+fetch("http://localhost:3000/users", configObj);
+.then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  })
+  .catch(function(error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  });
