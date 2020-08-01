22469f8a093a0096e6cf45614475188d83d7fed1
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-08-01 16:39:41 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+function submitData (name, email) {
+    let formData = {
+        name: name,
+        email: email
+    }
+    
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+
+    return fetch('http://localhost:3000/users', configObj)
+    .then(function(response) {
+         return response.json();
+    })
+    .then(function (json) {
+        const el = document.createElement('p');
+        el.innerHTML = json.id;
+        document.body.appendChild(el)
+    })
+    .catch(function(error) {
+        const el = document.createElement('p');
+        el.innerHTML = error.message;
+        document.body.appendChild(el)
+    })
+}
