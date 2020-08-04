affcd2a8c5a049790651c43ea2ff38e77afe64d6
ottocedeno <otto@creativespaceclub.com>
2020-08-04 17:51:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(userName, userEmail) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail,
+    }),
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      addObjectToDOM(object);
+    })
+    .catch(function (error) {
+      document.body.innerHTML = error.message;
+    });
+}
+
+function addObjectToDOM(object) {
+  document.body.innerHTML = object.id;
+}
