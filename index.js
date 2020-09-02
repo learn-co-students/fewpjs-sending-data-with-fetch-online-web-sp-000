f19be091b6c7193d0c5e3f719b5405aecb66b477
agiletkiewicz <a.letkiewicz@gmail.com>
2020-09-02 15:37:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email
+        })
+    })
+        .then(function(response) {
+            return response.json();
+          })
+          .then(function(object) {
+            print(object);
+          })
+          .catch(function(error) {
+            document.body.innerHTML += error.message;
+        
+        });
+}
+
+
+   
+function print(object) {
+    document.body.innerHTML += object.id;
+}
