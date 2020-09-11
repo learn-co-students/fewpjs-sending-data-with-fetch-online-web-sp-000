d682607519f13dfbfa9f3eb965d5c99a1e4631e3
Scott Hoisington <hoyzington@gmail.com>
2020-09-10 22:28:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      'name': name,
+      'email': email
+    })
+  })
+    .then(function(resp) {
+      return resp.json()
+    })
+    .then(function(obj) {
+      let div = document.createElement('div')
+      div.textContent = obj.id
+      document.body.appendChild(div)
+    })
+    .catch(function(err) {
+      let div = document.createElement('div')
+      div.textContent = err.message
+      document.body.appendChild(div)
+    })
+}
