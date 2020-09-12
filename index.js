eec0a3290288efd0b3786a7396b39ecb1a398253
ramseychapin <ramsey@ramseychapin.com>
2020-09-12 15:00:39 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(name, email) {
+	let formData = {
+		name: name,
+		email: email
+	};
+
+	let configurationObj = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Accept": "application/json"
+		},
+		body: JSON.stringify(formData)
+	};
+
+	return fetch("http://localhost:3000/users", configurationObj)
+	.then( (response) => {
+		return response.json()
+	})
+	.then( (object) => {
+		let h2 = document.createElement('h2');
+		h2.innerHTML = object.id;
+		document.body.appendChild(h2);
+		console.log(object);
+	})
+	.catch( (error) => {
+		let h3 = document.createElement('h3');
+		h3.innerHTML = error.message;
+		document.body.appendChild(h3);
+		console.log(error.message);
+	});
+}
