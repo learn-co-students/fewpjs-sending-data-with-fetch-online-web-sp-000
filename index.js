2a5285ca1b44c218bf138ae9714e2eb76ddc4830
ting682 <tchung682@gmail.com>
2020-09-18 11:35:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(username, useremail) {
+    let formData = {
+        name: username,
+        email: useremail
+      };
+       
+      // method: "POST" is missing from the object below
+      let configObj = {
+          method: "POST",
+        headers: {
+           
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+       
+      return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(object) {
+          //console.log(object);
+            
+            document.body.innerHTML = JSON.stringify(object)
+        })
+        .catch(function(error) {
+          alert("Unauthorized Access");
+          document.body.innerHTML = 'Unauthorized Access'
+          console.log(error.message);
+        });
+}
