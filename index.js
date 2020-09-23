9d69e4f66d0a1a4302750f558adf137230011810
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-09-23 05:22:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+
+function submitData(formData) {
+
+  let formData = {
+    name: "Jennifer Beaver",
+    email: "jen.lee.beaver@gmail.com"
+  };
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type":
+      "application/json",
+    },
+    body: JSON.stringify
+  };
+
+  fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log(object);
+    })
+    .catch(function(error) {
+      alert("There has been an error.");
+      console.log(error.message);
+    });
+    return fetch 
+}
