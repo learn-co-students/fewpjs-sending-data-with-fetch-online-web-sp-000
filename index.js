edc9259fdae7f1f8b0ed4e3a9dd6fe74976e3ef2
carrollm2 <carrollm2@gmail.com>
2020-09-27 22:51:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email){
+    
+    let configObj = {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+        },
+        body: JSON.stringify({name, email})
+    };
+    
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            document.body.innerHTML = object.id
+
+        })
+        .catch(function(error) {
+            alert("Error! Please retry again.");
+            document.body.innerHTML = error.message;
+        });
+}
