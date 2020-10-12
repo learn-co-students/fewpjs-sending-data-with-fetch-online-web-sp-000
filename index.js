51c091d7839a307ffb05e0ab31d33f132a8196d8
cantstopcoding <jonathan_koss@yahoo.com>
2020-10-12 12:51:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email) {
+    let url = 'http://localhost:3000/users';
+    let formData = {
+        name,
+        email
+    };
+    
+    let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+    return fetch(url, configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      // console.log(object.id);
+      document.body.innerHTML = object.id;
+    })
+    .catch(function(error) {
+      document.body.innerHTML = error.message;
+    });
