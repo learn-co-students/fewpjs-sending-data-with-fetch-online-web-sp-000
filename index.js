395f8645b079001d985b97c0d4092e7f92f564bf
David Strauch <dmaster18@gmail.com>
2020-11-05 20:46:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+let userData = {
+  name: "Steve",
+  email: "steve@steve.com"
+};
+
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(userData)
+}
+
+const userURL = 'http://localhost:3000/users'
+const body = document.querySelector('body');
+const idElement = document.createElement('div');
+const errorMessage = document.createElement('div');
+
+let submitData = function() {
+  fetch(userURL, configObj).then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    const id = object["id"];
+    idElement.innerHTML = `${id}`
+    body.appendChild(idElement);
+  })
+  .catch(function(error) {
+    alert("Unauthorized Access");
+    error.message = "Unauthorized Access";
+    errorMessage.innerHTML = "<p>" + error.message + "</p>";
+    body.appendChild(errorMessage);
+  });
+}
