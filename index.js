092bf8bf20ac5c4aee2fac4ca2a9ed5723d7a37a
Yumacorn <ReachRyanMui@gmail.com>
2020-11-28 10:07:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your code here
+function postDog() {
+    fetch("http://localhost:3000/dogs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        dogName: "Byron",
+        dogBreed: "Poodle"
+      })
+    });
+}
+
+document.querySelector('h1').addEventListener('click', postDog());
