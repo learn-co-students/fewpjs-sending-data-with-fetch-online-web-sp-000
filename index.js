4f2023ccc7f63ecf7777bfd251ae694fb860707a
keelsk <keelsk@gmail.com>
2020-12-28 11:32:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your code here
+function submitData(username, userEmail){
+    let configData = {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify({
+            name: username,
+            email: userEmail
+        })
+    };
+
+    return fetch("http://localhost:3000/users", configData)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(json){
+        showId(json.id);
+    })
+    .catch(function(error){
+        appendMessage(error);
+    });
+}
+
+function showId(id){
+    let p = document.createElement('p');
+    p.innerHTML= id;
+    document.body.appendChild(p);
+}
+
+function appendMessage(error){
+    let p = document.createElement('p');
+    p.innerHTML = error.message;
+    document.body.appendChild(p);
+}
