865fc552c1b38d88a56e86edb7f700d884ced5d5
seth <seth.j.near@gmail.com>
2021-01-10 14:33:01 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Add your code here
+// const { default: fetch } = require("node-fetch");
+
+// // Add your code here
+// fetch("http://localhost:3000/dogs", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify({
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   })
+// });
+
+
+function submitData (name, email) {
+return fetch("http://localhost:3000/users", {
+method: "POST",
+headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+},
+body: JSON.stringify({
+name: name,
+email: email
+
+})
+
+})
+.then(response => response.json())
+.then(object => {
+   let textNode =document.createTextNode(object.id);
+    // document.body.innerHTML = `<h1>${object.id}<h1/>`;
+    // let page = document.getElementsByTagName('body');
+   let h1 = document.createElement('h1');
+   h1.appendChild(textNode);
+document.getElementsByTagName('body')[0].appendChild(h1);
+    // let h1 = document.createElement('h1');
+    // h1.innerText = object.id;
+    // page.append(h1);
+})
+.catch( errorObj => {
+
+    let textNode = document.createTextNode(errorObj.message);
+   
+   let h1 = document.createElement('h1');
+   h1.appendChild(textNode);
+document.getElementsByTagName('body')[0].appendChild(h1);
+
+
+}
+
+
+
+)
+}
+
+// submitData('seth', 'seth.j.near@gmail.com');
