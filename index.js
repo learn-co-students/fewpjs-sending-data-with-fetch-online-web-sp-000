b8f7d472addf37afe366369b9843e9b38135a4aa
David Strauch <dmaster18@gmail.com>
2021-01-24 04:51:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
 // Add your code here
-let userData = {
-  name: "Steve",
-  email: "steve@steve.com"
-};
-
-let configObj = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify(userData)
-}
-
-const userURL = 'http://localhost:3000/users'
-const body = document.querySelector('body');
-const idElement = document.createElement('div');
-const errorMessage = document.createElement('div');
-
-let submitData = function() {
-  fetch(userURL, configObj).then(function(response) {
-    return response.json();
-  })
-  .then(function(object) {
-    const id = object["id"];
-    idElement.innerHTML = `${id}`
-    body.appendChild(idElement);
-  })
-  .catch(function(error) {
-    alert("Unauthorized Access");
-    error.message = "Unauthorized Access";
-    errorMessage.innerHTML = "<p>" + error.message + "</p>";
-    body.appendChild(errorMessage);
-  });
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+    },
+    body: JSON.stringify({name, email})
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        document.body.innerHTML = object.id;
+    })
+    .catch(function(error) {
+        document.body.innerHTML = error.message;
+    });
+    
 }
