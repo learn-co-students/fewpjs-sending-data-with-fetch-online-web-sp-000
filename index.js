7792e62f7040d74338dfd5f80fe67f32970a6fa1
anpham148 <anpham148@gmail.com>
2021-02-03 17:05:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email) {
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+      };
+       
+    return  fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(object) {
+          const body = document.querySelector('body')
+          const p = document.createElement('p')
+          p.innerHTML = object['id']
+          body.appendChild(p)
+        })
+        .catch(function(error) {
+            document.body.innerHTML = error.message;
+        });
+}
