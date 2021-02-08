03a7566c3e0ce0653cff8878184dc033fc7a79d2
khoinguyenkc <khoinguyenkc@gmail.com>
2021-02-08 10:56:24 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+function submitData(name, email) {
+    let data = {
+        name: name,
+        email: email
+    };
+
+    let configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(data)
+    };
+
+    //i think due to closure or something, this fetch will get access to the 2 variables above....
+
+    return fetch('http://localhost:3000/users', configObject).
+    then( function(response) { return response.json() }).
+    then( function(object) { renderResponse(object)  }).
+    catch( function(error) { renderErrorMessage( error.message)  })
+};
+
+function renderResponse(object) {
+    let bodyElement = document.getElementsByTagName("body")[0]
+    bodyElement.innerHTML = `id: ${object.id}`;
+    //the test would only accept directly the id in the body element... very rigid
+}
+
+function renderErrorMessage(message) {
+    let bodyElement = document.getElementsByTagName("body")[0]
+    bodyElement.innerHTML = `error: ${message}`;
+
+}
