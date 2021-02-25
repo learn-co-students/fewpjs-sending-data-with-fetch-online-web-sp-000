a22c188636cb6471a8c1cc1cbc98d42a668f187e
yarvinh <yarvinhhernandez@gmail.com>
2021-02-24 22:11:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+const url = 'http://localhost:3000/users'
+const name = "valentino"
+const email = "valentino@gmail.com"
+const body = document.querySelector('body')
+function submitData(name,email){
+   return fetch(url, {
+       method: "POST",
+       headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+       },
+       body: JSON.stringify({name: name, email: email})
+   })
+   .then(response =>{
+    return response.json()
+   })
+   .then(data => {
+       renderUser(data)
+       return data
+   })
+   .catch(error =>{
+    body.innerHTML  = error.message
+   })
+}
+function renderUser(json){
+body.innerHTML = `<h2> ${json.id} </h2>`
+}
+// submitData(name,email)
