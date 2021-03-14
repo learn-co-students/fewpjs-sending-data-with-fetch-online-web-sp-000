f31093f1f5f74ff49a53153f9132517123b9fb0a
Carrie Mathieu <carrieburks@gmail.com>
2021-03-13 22:24:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    let formData = {
+        name: userName,
+        email: userEmail
+    };
+       
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            document.body.innerHTML = (object['id']);
+        })
+        .catch(function(error) {
+            alert('Unauthorized Access')
+            document.body.innerHTML = error.message
+        })
+
+}
+
+
+   
+    
+    
