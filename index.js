a7a2d7d6324ca28cff33aa1fba5c08da846075bd
RSigmaSG <sg.comparch@gmail.com>
2021-03-18 21:26:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+function submitData() {
+    const formData = {
+        name: "Steve",
+        email: "steve@steve.com"
+      };
+       
+      const configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+       
+      const data = fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(object) {
+            let p = document.createElement('p');
+            p.innerHTML = object.id
+            document.body.appendChild(p);
+        })
+        .catch(function(error) {
+            let p = document.createElement('p');
+            p.innerHTML = "Unauthorized Access"
+            document.body.appendChild(p);
+          });
+          return data;
+}
+submitData();
