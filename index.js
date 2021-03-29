fbba83a3b40442e69983c2c0ad6c2045a703c4fb
TrueC-D <aletheacary@gmail.com>
2021-03-29 00:40:51 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(userNameInput, emailInput){
+    let addToBody = {
+        name: userNameInput,
+        email: emailInput}
+    return fetch('http://localhost:3000/users',{
+        method: "POST",
+        headers: {"Content-Type":"application/json", "Accept":"application/json"},
+        body: JSON.stringify(addToBody)
+    }).then(
+        function(response){
+            return response.json()
+        }).then(
+            function(object){
+                const userData = document.createElement('p')
+                userData.id = object.id
+                document.body.appendChild(userData)}
+            ).catch(function(error){
+                document.body.innerHTML = error.message})
+
+}
