d6e44cb5afb96436933b62b07e1f09cedd2b24a8
AlexJMartinez <alexandr3martin3z@gmail.com>
2021-03-30 16:16:15 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+
+function submitData(name, email) {
+
+    return fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+      name: name, 
+      email: email 
+  })
+})
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        const para = document.createElement("p")
+        document.body.appendChild(para)
+        para.innerHTML = object.id
+    })
+    .catch(function(error) {
+        alert("Unauthorized Access");
+        const para = document.createElement("p")
+        document.body.appendChild(para)
+        para.innerHTML = error.message
+    })
+
+};
+
