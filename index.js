11e94ed64e8adcfac13a3b778303671e9abbd0c1
Arodriguez1030 <arodriguez1030@gmail.com>
2021-04-01 15:57:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+fetch("http://localhost:3000/dogs", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept":  "applicattion/json"
+    },
+    body: JSON.stringify({
+        dogName: "Byron",
+        dogBreed: "Poodle"
+    })
+  });
+
+  function submitData(name, email) {
+      return fetch("http://localhost:3000/users",  {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json"
+          },
+          body: JSON.stringify({
+              name,
+              email
+          })
+      })
+      .then(function(response){
+          return response.json();
+      })
+      .then(function(object) {
+          document.body.innerHTML = object["id"];
+      })
+      .catch(function(error) {
+          alert("Uh oh!")
+          document.body.innerHTML = error.message
+      })
+  }
