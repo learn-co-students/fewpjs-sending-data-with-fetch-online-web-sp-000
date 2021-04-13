42c150bd0c0064a506bc623cf112a349390a3f01
acagirici <acagirici23@gmail.com>
2021-04-13 15:36:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const formData = {
+      name: userName,
+      email: userEmail
+    };
+ 
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+ 
+    return fetch("http://localhost:3000/users", configObj)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        console.log(object);
+        console.log(object["id"]);
+        const newP = document.createElement('p');
+        newP.innerHTML = object["id"];
+        document.body.appendChild(newP);
+      })
+      .catch(function(error) {
+        let message = 'Unauthorized Access'
+        const errorP = document.createElement('p');
+        errorP.innerHTML = message;
+        document.body.appendChild(errorP);
+      });
+ 
+  };
+
+
