adc7e252e9a81b93773e35f0a0b95b06aa4400c0
dbrandt1990 <dvbrandt90@gmail.com>
2021-05-19 13:54:12 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+
+function submitData(name, email) {
+    const userObj = {
+        name: name,
+        email: email
+    }
+
+    const configObj = {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "accept": "application/json"
+        },
+        body: JSON.stringify(userObj)
+    }
+    return fetch("http://localhost:3000/users", configObj)
+        .then(response => response.json())
+        .then(user => {
+            document.querySelector("body").append(user.id + " ")
+        }).catch(e => {
+            document.querySelector("body").append(e.message)
+        })
+}
