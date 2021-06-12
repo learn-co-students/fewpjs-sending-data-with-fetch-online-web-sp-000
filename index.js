864289bba26ce948575606bff6b20a3d8085b414
russell-macalintal <russell-macalintal@gmail.com>
2021-06-12 04:03:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json", 
+            "Accept": "application/json"
+        },
+        body: JSON.stringify( {name: userName, email: userEmail} )
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(response => response.json())
+        .then(function(object) {
+            const newElem = document.createElement("div");
+            const body = document.querySelector("body");
+            newElem.innerHTML = object["id"];
+            body.appendChild(newElem)})
+        .catch(function(error) {
+            const newError = document.createElement("div");
+            const body = document.querySelector("body");
+            newError.innerHTML = error.message;
+            body.appendChild(newError)});
+}
